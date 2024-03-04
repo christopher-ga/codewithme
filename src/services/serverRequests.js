e566eee60478392fc00697a2ba568280e51c2ef7
Christopher Garcia <chrisga@sas.upenn.edu>
2024-03-03 20:51:07 -0600

    updated server requests service file

--- src/services/serverRequests.js
+++ src/services/serverRequests.js
@@ -1,6 +1,6 @@
 const hostUrl = import.meta.env.VITE_REACT_APP_HOST_URL;
 
-const deletePageRequest = async (pageID, userPages, setUserPages) => {
+const deletePageRequest = async (pageID) => {
     const response = await fetch(`${hostUrl}/deletepage?pageId=${pageID}`)
 
     if (!response.ok) {
@@ -10,4 +10,62 @@ const deletePageRequest = async (pageID, userPages, setUserPages) => {
     return 'deleted page'
 }
 
-export {deletePageRequest}
+const fetchUserPagesRequest = async (userID) => {
+    const response = await fetch(`${hostUrl}/getuserpages?userID=${userID}`)
+    if (!response.ok) {
+        return null
+    }
+     return await response.json();
+}
+
+const createNewPageRequest = async (userID, username) => {
+    const response = await fetch(`${hostUrl}/createpage`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            userID,
+            username
+        }),
+    });
+
+    if (!response.ok) {
+        return;
+    }
+
+    return await response.json();
+}
+
+const fetchUserSharedPagesRequest = async (username) => {
+    const response = await fetch(`${hostUrl}/getsharedpages?username=${username}`)
+
+    if (!response.ok) {
+        return;
+    }
+
+    return await response.json();
+}
+
+const sharePageRequest = async (shareWithUsername, pageID, username, title) => {
+    const response = await fetch(`${hostUrl}/sharepage?shareusername=${shareWithUsername}&pageID=${pageID}&ownerusername=${username}&title=${title}`)
+
+    if (!response.ok) {
+        console.log(await response.json())
+        return;
+    }
+
+    return response.json();
+}
+
+const fetchPagesSharedUserList = async (pageID) => {
+    const response = await fetch(`${hostUrl}/getuserssharingpage?pageID=${pageID}`);
+    if (!response.ok) {
+
+        return;
+    }
+    return await response.json();
+}
+
+export {sharePageRequest, fetchPagesSharedUserList, deletePageRequest, fetchUserPagesRequest, createNewPageRequest, fetchUserSharedPagesRequest}
+
